2591a084bd89064205d6d04d46d72ec058570b39
ElemarJR <elemar@procad.net>
2011-07-23 18:00:31 -0300

    Parte 08 - Basic HLSL

---
+++ Parte 08/src/XnaSpaceSimulator/XnaSpaceSimulator/XnaSpaceSimulator/ChaseCamera.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace XnaSpaceSimulator
+{
+    public class ChaseCamera : Camera
+    {
+        public Vector3 Position { get; private set; }
+        public Vector3 Target { get; private set; }
+
+        public Vector3 FollowTargetPosition { get; private set; }
+        public Vector3 FollowTargetRotation { get; private set; }
+
+        public Vector3 PositionOffset { get; set; }
+        public Vector3 TargetOffset { get; set; }
+
+        public Vector3 RelativeCameraRotation { get; set; }
+
+        float springiness = .015f;
+
+        public float Springiness
+        {
+            get { return springiness; }
+            set { springiness = MathHelper.Clamp(value, 0, 1); }
+        }
+
+        public ChaseCamera(Vector3 PositionOffset, Vector3 TargetOffset,
+            Vector3 RelativeCameraRotation, GraphicsDevice graphicsDevice)
+            : base(graphicsDevice)
+        {
+            this.PositionOffset = PositionOffset;
+            this.TargetOffset = TargetOffset;
+            this.RelativeCameraRotation = RelativeCameraRotation;
+        }
+
+        public void Move(
+            Vector3 newFollowTargetPosition,
+            Vector3 newFollowTargetRotation
+            )
+        {
+            this.FollowTargetPosition = newFollowTargetPosition;
+            this.FollowTargetRotation = newFollowTargetRotation;
+        }
+
+        public void Rotate(Vector3 RotationChange)
+        {
+            this.RelativeCameraRotation += RotationChange;
+        }
+
+        public override void Update()
+        {
+            Vector3 combinedRotation = FollowTargetRotation +
+                RelativeCameraRotation;
+
+            Matrix rotation = Matrix.CreateFromYawPitchRoll(
+                combinedRotation.Y, combinedRotation.X, combinedRotation.Z);
+
+
+            Vector3 desiredPosition = FollowTargetPosition +
+                Vector3.Transform(PositionOffset, rotation);
+
+            Position = Vector3.Lerp(Position, desiredPosition, Springiness);
+
+            Target = FollowTargetPosition +
+                Vector3.Transform(TargetOffset, rotation);
+
+            Vector3 up = Vector3.Transform(Vector3.Up, rotation);
+
+            View = Matrix.CreateLookAt(Position, Target, up);
+        }
+    }
+}
---
+++ Parte 08/src/XnaSpaceSimulator/XnaSpaceSimulator/XnaSpaceSimulator/GameModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XnaSpaceSimulator
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class GameModel
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
+
+        public Vector3 BaseRotation { get; set; }
+
+        public Model Model { get; private set; }
+
+        Matrix[] modelTransforms;
+
+        public GameModel(
+            Model Model
+            )
+        {
+            this.Model = Model;
+
+            modelTransforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(modelTransforms);
+
+            this.baseBoundingSphere = Model.ComputeBoundingSphere();
+
+            this.Position = Vector3.Zero;
+            this.Rotation = Vector3.Zero;
+            this.Scale = Vector3.One;
+        }
+
+        public static implicit operator GameModel(Model @model)
+        {
+            return new GameModel(@model);
+        }
+
+
+        private BoundingSphere baseBoundingSphere;
+        public BoundingSphere BoundingSphere
+        {
+            get
+            {
+                return baseBoundingSphere.Transform(
+                    ComputeWorld()
+                    );
+            }
+        }
+
+        Matrix ComputeWorld()
+        {
+            return
+                Matrix.CreateFromYawPitchRoll(
+                    BaseRotation.Y,
+                    BaseRotation.X,
+                    BaseRotation.Z
+                    ) *
+                Matrix.CreateScale(Scale) *
+                Matrix.CreateFromYawPitchRoll(
+                    Rotation.Y,
+                    Rotation.X,
+                    Rotation.Z
+                    ) *
+                Matrix.CreateTranslation(Position);
+        }
+
+        public void Draw(Matrix View, Matrix Projection)
+        {
+            Matrix baseWorld = ComputeWorld();
+
+
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                Matrix localWorld = modelTransforms[mesh.ParentBone.Index]
+                    * baseWorld;
+                mesh.SetupEffects(localWorld, View, Projection);
+                mesh.Draw();
+            }
+        }
+    }
+}
