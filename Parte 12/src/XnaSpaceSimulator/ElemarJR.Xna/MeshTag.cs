6a5fb6c370afe38d3ad43b9be224d10308c7bfaf
ElemarJR <elemar@procad.net>
2011-08-03 21:55:57 -0300

    Parte 12

---
+++ Parte 12/src/XnaSpaceSimulator/ElemarJR.Xna/MeshTag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ElemarJR.Xna
+{
+    class MeshTag
+    {
+        public Vector3 Color { get; private set; }
+        public Texture2D Texture { get; private set; }
+        public float SpecularPower { get; private set; }
+        public Effect CachedEffect { get; set; }
+
+        public MeshTag(
+            Vector3 color,
+            Texture2D texture,
+            float specularPower
+            )
+        {
+            this.Color = color;
+            this.Texture = texture;
+            this.SpecularPower = specularPower;
+        }
+    }
+}
