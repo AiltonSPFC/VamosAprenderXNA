b349d7915a61b9e2ac3b077e8123df335e3a8bd1
ElemarJR <elemar@procad.net>
2011-08-04 21:49:06 -0300

    Parte 13

---
+++ Parte 13/src/XnaSpaceSimulator/ElemarJR.Xna/Cameras/IPositionableCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ElemarJR.Xna.Cameras
+{
+    public interface IPositionableCamera
+    {
+        Vector3 Position { get;  }
+        Vector3 Target { get;  }
+
+        BoundingFrustum Frustum { get; }
+        bool IsInView(BoundingBox box);
+        bool IsInView(BoundingSphere sphere);
+        Matrix Projection { get; }
+        void Update();
+        Matrix View { get; }
+    }
+}
