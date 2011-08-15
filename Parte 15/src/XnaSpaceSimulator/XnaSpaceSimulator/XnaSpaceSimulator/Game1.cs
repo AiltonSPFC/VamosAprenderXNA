77f8e266372b83c85bf0be267eb0f52b3182fc64
ElemarJR <elemar@procad.net>
2011-08-15 10:05:07 -0300

    Code improvement

--- Parte 15/src/XnaSpaceSimulator/XnaSpaceSimulator/XnaSpaceSimulator/Game1.cs
+++ Parte 15/src/XnaSpaceSimulator/XnaSpaceSimulator/XnaSpaceSimulator/Game1.cs
@@ -21,23 +21,14 @@ namespace XnaSpaceSimulator
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
 
-
-        IPositionableCamera camera1;
-        IPositionableCamera camera2;
-
-        readonly List<IDrawableModel> models = new List<IDrawableModel>();
-        GameModel spaceship1;
-        GameModel spaceship2;
-
-        Viewport defaultViewport;
-        Viewport topViewport;
-        Viewport bottomViewport;
-
+        public readonly List<IDrawableModel> Models = new List<IDrawableModel>();
+        Spaceship spaceship1;
+        Spaceship spaceship2;
         Terrain ground;
 
-        //Effect groundEffect;
+        Viewport viewport1;
+        Viewport viewport2;
 
         public Game1()
         {
@@ -47,199 +38,100 @@ namespace XnaSpaceSimulator
 
         protected override void LoadContent()
         {
+#if DEBUG
             this.Components.Add(new FpsGameComponent(this, graphics));
+#endif
+            SetupViewports();
+            LoadSpaceship1();
+            LoadSpaceship2();
+            LoadGround();
+        }
 
-            var device = graphics.GraphicsDevice;
+        private void SetupViewports()
+        {
+            var defaultViewport = graphics.GraphicsDevice.Viewport;
+            viewport1 = defaultViewport;
+            viewport2 = defaultViewport;
 
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            viewport1.Width /= 2;
+            viewport1.Width--;
+            viewport2.Width /= 2;
+            viewport2.Width--;
 
-            spaceship1 = new GameModel(Content.Load<Model>("spaceship"))
-            {
-                Position = new Vector3(0, 3700, -2800),
-                Scale = new Vector3(50f),
-                BaseRotation = new Vector3(0, MathHelper.Pi, 0),
-                Rotation = new Vector3(0, MathHelper.Pi, 0)
-            };
+            viewport1.X =
+                Window.ClientBounds.Width - viewport2.Width;
+        }
 
-            models.Add(spaceship1);
-
-
-            spaceship2 = new GameModel(Content.Load<Model>("ship"))
-            {
-                Position = new Vector3(0, 3500, 4000),
-                Scale = new Vector3(0.7f),
-                BaseRotation = new Vector3(0, 0, 0),
-                Rotation = new Vector3(0, 0, 0)
-            };
-
-            models.Add(spaceship2);
-
+        private void LoadGround()
+        {
             var effect = Content.Load<Effect>("BasicTerrainEffect");
             effect.Parameters["Texture"].SetValue(Content.Load<Texture2D>("grass"));
 
             ground = new Terrain(
                 Content.Load<Texture2D>("heightmap1"),
                 effect,
-                30, 4800, device
+                30, 4800, this.GraphicsDevice
                 );
 
-            models.Add(ground);
-
-            defaultViewport = graphics.GraphicsDevice.Viewport;
-            topViewport = defaultViewport;
-            bottomViewport = defaultViewport;
-
-            //topViewport.Height /= 2;
-            //topViewport.Height--;
-            //bottomViewport.Height /= 2;
-            //bottomViewport.Height--;
-            //bottomViewport.Y =
-            //    Window.ClientBounds.Height - bottomViewport.Height;
-
-            topViewport.Width /= 2;
-            topViewport.Width--;
-            bottomViewport.Width /= 2;
-            bottomViewport.Width--;
-
-            bottomViewport.X =
-                Window.ClientBounds.Width - bottomViewport.Width;
-
-            camera1 = new ChaseCamera(
-                new Vector3(0, 400, 1500),
-                new Vector3(0, 200, 0),
-                new Vector3(0, 0, 0),
-                GraphicsDevice,
-                new Vector3(0, 3700, -2800), topViewport);
-
-            camera2 = new ChaseCamera(
-                new Vector3(0, 400, 1500),
-                new Vector3(0, 200, 0),
-                new Vector3(0, 0, 0),
-                GraphicsDevice,
-                new Vector3(0, 3500, 4000), bottomViewport);
-
-
+            Models.Add(ground);
         }
 
-        protected override void UnloadContent()
+        private void LoadSpaceship2()
         {
+            spaceship2 = new Spaceship(this, new GameModel(Content.Load<Model>("ship"))
+            {
+                Position = new Vector3(0, 3500, 4000),
+                Scale = new Vector3(0.7f),
+                BaseRotation = new Vector3(0, 0, 0),
+                Rotation = new Vector3(0, 0, 0)
+            }, viewport2)
+            {
+                Up = Keys.R,
+                Down = Keys.F,
+                Left = Keys.A,
+                Right = Keys.D,
+                Forward = Keys.W,
+                Backward = Keys.S
+            };
+
+            this.Components.Add(spaceship2);
+            Models.Add(spaceship2.Model);
         }
 
-        protected override void Update(GameTime gameTime)
+        private void LoadSpaceship1()
         {
-            UpdateSpaceship1(gameTime);
-            UpdateSpaceship2(gameTime);
-            UpdateCamera(gameTime);
-
-            base.Update(gameTime);
-        }
-
-        void UpdateSpaceship1(GameTime gameTime)
-        {
-            KeyboardState keyState = Keyboard.GetState();
-
-            Vector3 rotChange = new Vector3(0, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.PageUp))
-                rotChange += new Vector3(1, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.PageDown))
-                rotChange += new Vector3(-1, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.Left))
-                rotChange += new Vector3(0, 1, 0);
-
-            if (keyState.IsKeyDown(Keys.Right))
-                rotChange += new Vector3(0, -1, 0);
-
-            spaceship1.Rotation += rotChange * .025f;
-
-            if (!keyState.IsKeyDown(Keys.Up) &&
-                !keyState.IsKeyDown(Keys.Down))
-                return;
-
-            Matrix rotation = Matrix.CreateFromYawPitchRoll(
-                spaceship1.Rotation.Y, spaceship1.Rotation.X, spaceship1.Rotation.Z
-                );
-
-            spaceship1.Position +=
-                Vector3.Transform(
-                    keyState.IsKeyDown(Keys.Up) ? Vector3.Forward : Vector3.Backward,
-                    rotation
-                    )
-                * (float)gameTime.ElapsedGameTime.TotalMilliseconds * 4;
-        }
-
-        void UpdateSpaceship2(GameTime gameTime)
-        {
-            KeyboardState keyState = Keyboard.GetState();
-
-            Vector3 rotChange = new Vector3(0, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.R))
-                rotChange += new Vector3(1, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.F))
-                rotChange += new Vector3(-1, 0, 0);
-
-            if (keyState.IsKeyDown(Keys.A))
-                rotChange += new Vector3(0, 1, 0);
-
-            if (keyState.IsKeyDown(Keys.D))
-                rotChange += new Vector3(0, -1, 0);
-
-            spaceship2.Rotation += rotChange * .025f;
-
-            if (!keyState.IsKeyDown(Keys.W) &&
-                !keyState.IsKeyDown(Keys.S))
-                return;
-
-            Matrix rotation = Matrix.CreateFromYawPitchRoll(
-                spaceship2.Rotation.Y, spaceship2.Rotation.X, spaceship2.Rotation.Z
-                );
-
-            spaceship2.Position +=
-                Vector3.Transform(
-                    keyState.IsKeyDown(Keys.W) ? Vector3.Forward : Vector3.Backward,
-                    rotation
-                    )
-                * (float)gameTime.ElapsedGameTime.TotalMilliseconds * 4;
-        }
-
-        void UpdateCamera(GameTime gameTime)
-        {
-            KeyboardState keyState = Keyboard.GetState();
-
-            var chase = camera1 as ChaseCamera;
-            chase.Move(
-                spaceship1.Position,
-                spaceship1.Rotation
-                );
-            camera1.Update();
-
-
-            chase = camera2 as ChaseCamera;
-            chase.Move(
-                spaceship2.Position,
-                spaceship2.Rotation
-                );
-
-            camera2.Update();
+            spaceship1 = new Spaceship(this, new GameModel(Content.Load<Model>("spaceship"))
+            {
+                Position = new Vector3(0, 3700, -2800),
+                Scale = new Vector3(50f),
+                BaseRotation = new Vector3(0, MathHelper.Pi, 0),
+                Rotation = new Vector3(0, MathHelper.Pi, 0)
+            }, viewport1)
+            {
+                Up = Keys.PageUp,
+                Down = Keys.PageDown,
+                Left = Keys.Left,
+                Right = Keys.Right,
+                Forward = Keys.Up,
+                Backward = Keys.Down
+            };
+            this.Components.Add(spaceship1);
+            Models.Add(spaceship1.Model);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Viewport = topViewport;
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            foreach (IDrawableModel model in models)
-                model.Draw(camera1.View, camera1.Projection);
-
-
-            GraphicsDevice.Viewport = bottomViewport;
-            foreach (IDrawableModel model in models)
-                model.Draw(camera2.View, camera2.Projection);
-
+            DrawScene(spaceship1.Camera);
+            DrawScene(spaceship2.Camera);
+            
             base.Draw(gameTime);
+        }
+
+        public void DrawScene(Camera camera)
+        {
+            GraphicsDevice.Viewport = camera.Viewport;
+            foreach (IDrawableModel model in Models)
+                model.Draw(camera.View, camera.Projection);
         }
     }
 }
---
+++ Parte 15/src/XnaSpaceSimulator/XnaSpaceSimulator/XnaSpaceSimulator/Spaceship.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XnaSpaceSimulator
+{
+    using ElemarJR.Xna;
+    using ElemarJR.Xna.Cameras;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    class Spaceship : GameComponent
+    {
+        public ChaseCamera Camera { get; private set; }
+        public GameModel Model { get; private set; }
+        
+        public Keys Left { get; set; }
+        public Keys Right { get; set; }
+        public Keys Forward { get; set; }
+        public Keys Backward { get; set; }
+        public Keys Up { get; set; }
+        public Keys Down { get; set; }
+
+        public Spaceship(
+            Game game, 
+            GameModel model, 
+            Viewport viewport
+            )
+            : base(game)
+        {
+            this.Model = model;
+            this.Camera = new ChaseCamera(
+                new Vector3(0, 400, 1500),
+                new Vector3(0, 200, 0),
+                new Vector3(0, 0, 0),
+                game.GraphicsDevice,
+                model.Position, viewport);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            UpdateCamera();
+            UpdatePosition(gameTime);
+            base.Update(gameTime);
+        }
+
+        void UpdateCamera()
+        {
+            this.Camera.Move(
+                Model.Position,
+                Model.Rotation
+                );
+
+            this.Camera.Update();
+        }
+
+        void UpdatePosition(GameTime gameTime)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+
+            Vector3 rotChange = new Vector3(0, 0, 0);
+
+            if (keyState.IsKeyDown(Up))
+                rotChange += new Vector3(1, 0, 0);
+
+            if (keyState.IsKeyDown(Down))
+                rotChange += new Vector3(-1, 0, 0);
+
+            if (keyState.IsKeyDown(Left))
+                rotChange += new Vector3(0, 1, 0);
+
+            if (keyState.IsKeyDown(Right))
+                rotChange += new Vector3(0, -1, 0);
+
+            Model.Rotation += rotChange * .025f;
+
+            if (!keyState.IsKeyDown(Forward) &&
+                !keyState.IsKeyDown(Backward))
+                return;
+
+            Matrix rotation = Matrix.CreateFromYawPitchRoll(
+                Model.Rotation.Y, Model.Rotation.X, Model.Rotation.Z
+                );
+
+            Model.Position +=
+                Vector3.Transform(
+                    keyState.IsKeyDown(Forward) ? Vector3.Forward : Vector3.Backward,
+                    rotation
+                    )
+                * (float)gameTime.ElapsedGameTime.TotalMilliseconds * 4;
+
+        }
+
+    }
+}
